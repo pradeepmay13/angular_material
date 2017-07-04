6728d09a9c4185055e92427fb728047ae6a5cd1f
Pradeep <pradeepmay13@gmail.com>
2017-07-05 00:29:11 +0530

    form submission

--- src/app/user-registration/user-registration.component.ts
+++ src/app/user-registration/user-registration.component.ts
@@ -2,10 +2,18 @@ import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 import { UserValidationService } from './user-validation.service';
 
+import { Http, Response }          from '@angular/http';
+ 
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
 //https://embed.plnkr.co/ukwCXm/
 //https://coryrylan.com/blog/angular-form-builder-and-validation-management
 //https://github.com/start-angular/SB-Admin-BS4-Angular-4
 //http://www.dotnetjalps.com/2016/07/angular2-packages-sublime-text3.html
+//http://dotsway.com/2017/04/05/simple-page-binding-in-angular-4/
+//https://scotch.io/tutorials/angular-2-http-requests-with-observables
 
 
 @Component({
@@ -17,27 +25,32 @@ import { UserValidationService } from './user-validation.service';
 	  	border-bottom: red;
 	}
   `],
-  providers:[UserValidationService]
+  providers : [ UserValidationService ]
 })
 export class UserRegistrationComponent implements OnInit {
-
-	userForm:FormGroup;	
-	constructor(private fb:FormBuilder) { }
+	userForm: FormGroup;
+	constructor(private fb: FormBuilder, private http: Http) { }
 
 	ngOnInit() {
-		this.userForm=this.fb.group({
+		this.userForm = this.fb.group({
 			username	:	['', [Validators.required, Validators.minLength(4)]],
 			email		:	['', Validators.compose([Validators.required, UserValidationService.emailValidator])],
 			firstname	:	['', [Validators.required]],
 			lastname	:	['', [Validators.required]],
 			address 	: this.fb.group({
-						addressline	:	['',[Validators.required]],
-						city		:	['',[Validators.required]],
-						country		:	['',[Validators.required]],
-						postalCode	:	['',[Validators.required, Validators.pattern('^[1-9][0-9]{4}$')]],
-			}),			
+						addressline	:	['', [Validators.required]],
+						city		:	['', [Validators.required]],
+						country		:	['', [Validators.required]],
+						postalCode	:	['', Validators.compose([Validators.required, Validators.pattern('^[1-9][0-9]{4}$')])],
+						}),
 			aboutYou	:	['', [Validators.required]],
 		})
 	}
-
+	onSubmit(value): Observable<registration[]> {
+		console.log(value);
+		private commentsUrl = 'http://localhost:3000/api/comments';
+		let bodyString = JSON.stringify(body); // Stringify payload
+        let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+        let options       = new RequestOptions({ headers: headers }); // Create a request option
+	}
 }
